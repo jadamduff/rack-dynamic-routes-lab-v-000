92a3febb3d65141bd37a78748cc470e4d55400fe
Adam Duff <j.adam.duff@gmail.com>
2018-09-01 05:01:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,13 @@
 class Application
 
-  @@item = []
-
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/items"
-      @@item.each do |x|
-        if x.name == req.params["item"]
+    if req.path.match("/items/")
+      item_title = req.path.split("/item/").last
+      Song.item.each do |x|
+        if x.name == item_title
           resp.write "#{x.price}"
         else
           resp.write "Item not found"
