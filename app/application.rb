c4f7015d9721b70ed8ad598eeaeb93edae7f796c
Adam Duff <j.adam.duff@gmail.com>
2018-09-01 05:13:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,10 +10,10 @@ class Application
         resp.write @@items.find {|x| x.name == item_title}.price
       else
         resp.write "Item not found"
-        resp.status 400
+        resp.status = 400
       end
     else
-      resp.status 404
+      resp.status = 404
     end
     resp.finish
   end
