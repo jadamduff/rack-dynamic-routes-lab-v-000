935c2c53085f8f7fb3e1e499002bb9770b0ddd3e
Adam Duff <j.adam.duff@gmail.com>
2018-09-01 05:06:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match("/items/")
+    if req.path.match(/items/)
       item_title = req.path.split("/item/").last
       @@items.each do |x|
         if x.name == item_title
